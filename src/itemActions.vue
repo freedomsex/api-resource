84bfb6573e6da96d78f212a261027dd457d5b961
tebaly <support@freedomsex.net>
2020-08-16 15:42:42 +0300

    fix: item delete

--- src/itemActions.vue
+++ src/itemActions.vue
@@ -11,6 +11,11 @@ export default {
       let {data} = await this.$api.res(name, api).put(params, id);
       return data;
     },
+    async delete(id) {
+      let {name, api} = this.resource;
+      let {data} = await this.$api.res(name, api).delete(id);
+      return data;
+    },
   },
 };
 </script>
